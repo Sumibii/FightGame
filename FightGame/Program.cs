dbddc87af125b06ef6bf1ee81a41cfee941af255
Sumibii <filiptheking13@gmail.com>
2025-09-16 14:42:00 +0200

    Added class selection, implemented hp check before letting monster attack to prevent ties. This resulted in tie mechanic being removed. Prevented HP from going under zero resulting in ugly results.

--- FightGame/Program.cs
+++ FightGame/Program.cs
@@ -9,24 +9,53 @@ To do: Make hp unable to go under 0, Class selection (might need to learn classe
 
 
 
-
-
-
-
-
-
-
-
 int PlayerHP = 100;
 int MonsterHP = 100;
 string name = "";
 string monster = "Curly Haired Baboon";
 int Round = 1;
-
+int PlayerMinDmg = 10;
+int PlayerMaxDmg = 31;
 
 Console.WriteLine("Welcome to the Fighting Game!");
 Console.WriteLine("What's your name?");
 name = Console.ReadLine();
+
+string build = "";
+Console.WriteLine("Choose your class!");
+Console.WriteLine("1, Glasscannon(1 HP, 99-100 DMG)");
+Console.WriteLine("2, Balance(100 HP, 10-30 DMG)");
+Console.WriteLine("3, Tank (175 HP, 5-15 DMG)");
+Console.WriteLine("4, Unpenetrable Fortress (5000 HP, 1 DMG)");
+while (build != "1" && build != "2" && build != "3" && build != "4")
+{
+    build = Console.ReadLine();
+}
+if (build == "1")
+{
+    PlayerHP -= 99;
+    PlayerMinDmg += 89;
+    PlayerMaxDmg += 70;
+    Console.WriteLine("Classcannon chosen!");
+}
+else if (build == "2")
+{
+    Console.WriteLine("Balance chosen!");
+}
+else if (build == "3")
+{
+    PlayerHP += 75;
+    PlayerMinDmg -= 5;
+    PlayerMaxDmg -= 15;
+    Console.WriteLine("Tank chosen!");
+}
+else if (build == "4")
+{
+    PlayerHP += 4900;
+    PlayerMinDmg -= 9;
+    PlayerMaxDmg -= 29;
+    Console.WriteLine("Unpenetrable Fortress chosen!");
+}
 
 while (PlayerHP > 0 && MonsterHP > 0)
 {
@@ -36,32 +65,37 @@ while (PlayerHP > 0 && MonsterHP > 0)
     Console.WriteLine($"{name}'s HP:{PlayerHP}, Monster HP: {MonsterHP}");
     Console.WriteLine("");
 
-    int PlayerDMG = Random.Shared.Next(PlayerMinDmg, PlayerMaxDmg   );
+    int PlayerDMG = Random.Shared.Next(PlayerMinDmg, PlayerMaxDmg);
     Console.WriteLine($"{name}'s turn!");
     MonsterHP -= PlayerDMG;
-    if (MonsterHP < 0) MonsterHP = 0;
+    if (MonsterHP < 0) MonsterHP = 0; // Prevents hp going under 0, i.e. -20 hp
     Console.WriteLine($"{name} did {PlayerDMG} DMG");
+    if (MonsterHP <= 0)
+    {
+        Console.WriteLine($"{monster} has been defeated!");
+        break;
+    }
 
-    int MonsterDMG = Random.Shared.Next(10, 30);
-    Console.WriteLine("");
-    Console.WriteLine($"{monster}'s turn!");
-    PlayerHP -= MonsterDMG;
-    if (PlayerHP < 0) PlayerHP = 0;
-    Console.WriteLine($"{monster} did {MonsterDMG} DMG");
-    Round++;
-    Console.WriteLine("");
-    Console.WriteLine("Press a button to continue");
-    Console.ReadKey();
-    Console.WriteLine("");
+        int MonsterDMG = Random.Shared.Next(10, 30);
+        Console.WriteLine("");
+        Console.WriteLine($"{monster}'s turn!");
+        PlayerHP -= MonsterDMG;
+        if (PlayerHP < 0) PlayerHP = 0; //same thing as before (Line 71)
+        Console.WriteLine($"{monster} did {MonsterDMG} DMG");
+        Round++;
+        Console.WriteLine("");
+        Console.WriteLine("Press a button to continue");
+        Console.ReadKey();
+        Console.WriteLine("");
 }
 
 Console.WriteLine("Fight over!");
 Console.WriteLine($"Player HP:{PlayerHP}, Monster HP: {MonsterHP}");
-if (PlayerHP == 0 && MonsterHP == 0)
-{
-    Console.WriteLine("Draw");
-}
-else if (PlayerHP >= 0 && MonsterHP == 0)
+/*
+Removed tie mechanic as it should be patched
+*/
+
+if (PlayerHP >= 0 && MonsterHP == 0)
 {
     Console.WriteLine("You won");
 }
